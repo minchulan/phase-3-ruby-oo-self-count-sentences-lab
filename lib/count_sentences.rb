679b092ba45eef0ed586dbf81aad9e734fd4f041
Minchul An <min@minchulan.io>
2023-01-31 15:45:32 -0700

    commit tree 9350b3a06488d1e23a0987d64de94a63fed086c3

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,34 @@ require 'pry'
 class String
 
   def sentence?
-    
-  end
+    if self.end_with?(".")
+      true
+    else
+      false 
+    end 
+  end 
 
   def question?
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
 
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    self.scan(/[^\.!?]+[\.!?]/).map(&:strip).count
   end
 end
+
+# string.scan(/[^\.!?]+[\.!?]/).map(&:strip).count # scan has regex to split string and strip will remove trailing spaces.
+# # => 4 
